3d0799efa195165027b77a480142964cdb25df81
sjhjack <102958758+sjhjack@users.noreply.github.com>
2024-02-12 23:10:56 +0900

    [Bronze V] Title: Rook, Time: 120 ms, Memory: 13936 KB -BaekjoonHub

---
+++ 백준/Bronze/2393. Rook/Rook.java
@@ -0,0 +1,20 @@
+public class Main {
+    public static void main(String[] args) {
+		StringBuilder ans = new StringBuilder();
+		ans.append("  ___  ___  ___").append("\n");
+		ans.append("  | |__| |__| |").append("\n");
+		ans.append("  |           |").append("\n");
+		ans.append("   \\_________/").append("\n");
+		ans.append("    \\_______/").append("\n");
+		ans.append("     |     |").append("\n");
+		ans.append("     |     |").append("\n");
+		ans.append("     |     |").append("\n");
+		ans.append("     |     |").append("\n");
+		ans.append("     |_____|").append("\n");
+		ans.append("  __/       \\__").append("\n");
+		ans.append(" /             \\").append("\n");
+		ans.append("/_______________\\").append("\n");
+
+		System.out.print(ans);
+	}
+}
