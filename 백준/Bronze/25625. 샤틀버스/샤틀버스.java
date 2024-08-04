3d3c142e4fae480e5b5e31b92c9df60d80b56ffb
sjhjack <102958758+sjhjack@users.noreply.github.com>
2024-08-04 12:24:50 +0900

    [Bronze IV] Title: 샤틀버스, Time: 104 ms, Memory: 14160 KB -BaekjoonHub

---
+++ 백준/Bronze/25625. 샤틀버스/샤틀버스.java
@@ -0,0 +1,14 @@
+import java.util.*;
+import java.io.*;
+
+class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        int x = Integer.parseInt(st.nextToken());
+        int y = Integer.parseInt(st.nextToken());
+
+        System.out.print(x > y ? x + y : y - x);
+    }
+}
