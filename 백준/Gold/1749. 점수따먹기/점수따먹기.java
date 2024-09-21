20bdb4a7ca4d29f149f126878e03f86fd1c6f19f
sjhjack <102958758+sjhjack@users.noreply.github.com>
2024-09-21 12:14:51 +0900

    [Gold IV] Title: 점수따먹기, Time: 800 ms, Memory: 21212 KB -BaekjoonHub

---
+++ 백준/Gold/1749. 점수따먹기/점수따먹기.java
@@ -0,0 +1,56 @@
+import java.io.*;
+import java.util.*;
+
+class Main {
+    static int N, M;
+    static int[][] arr;
+    static int[][] horizonPrefSum;
+    
+    public static void main(String[] args) throws IOException {
+        init();
+        solve();
+    }
+
+    static void init() throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        N = Integer.parseInt(st.nextToken());
+        M = Integer.parseInt(st.nextToken());
+
+        arr = new int[N + 1][M + 1];
+        horizonPrefSum = new int[N + 1][M + 1];
+        
+        for(int i = 1; i <= N; i++) {
+            st = new StringTokenizer(br.readLine());
+            for(int j = 1; j <= M; j++) {
+                arr[i][j] = Integer.parseInt(st.nextToken());
+            }
+        }
+
+        for(int i = 1; i <= N; i++) {
+            for(int j = 1; j <= M; j++) {
+                horizonPrefSum[i][j] = horizonPrefSum[i][j-1] + arr[i][j];
+            }
+        }
+    }
+
+    static void solve() {
+        int ans = Integer.MIN_VALUE;
+
+        for(int i = 1; i <= N; i++) {
+            for(int j = 1; j <= M; j++) {
+                
+                for(int k = 1; k <= j; k++) {
+                    int sum = 0;
+                    for(int l = 0; l < i; l++) {
+                        sum += horizonPrefSum[i-l][j] - horizonPrefSum[i-l][j-k];
+                        ans = Math.max(ans, sum);
+                    }
+                }
+            }
+        }
+
+        System.out.print(ans);
+    }
+}
