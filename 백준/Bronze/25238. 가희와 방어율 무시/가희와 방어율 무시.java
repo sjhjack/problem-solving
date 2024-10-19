923925b6b0ada8ca46cfd949f30f80ae7d62a89b
sjhjack <102958758+sjhjack@users.noreply.github.com>
2024-10-19 22:11:08 +0900

    [Bronze IV] Title: 가희와 방어율 무시, Time: 100 ms, Memory: 14144 KB -BaekjoonHub

---
+++ 백준/Bronze/25238. 가희와 방어율 무시/가희와 방어율 무시.java
@@ -0,0 +1,13 @@
+import java.io.*;
+import java.util.*;
+
+class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int a = Integer.parseInt(st.nextToken());
+        int b = Integer.parseInt(st.nextToken());
+
+        System.out.print(a - a * (1.0 * b / 100) >= 100 ? 0 : 1);
+    }
+}
