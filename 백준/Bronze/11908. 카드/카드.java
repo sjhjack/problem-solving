150b488a6780b8b8e1a6a974c3ae3938a9160835
sjhjack <102958758+sjhjack@users.noreply.github.com>
2024-12-22 18:32:22 +0900

    [Bronze III] Title: 카드, Time: 112 ms, Memory: 14572 KB -BaekjoonHub

---
+++ 백준/Bronze/11908. 카드/카드.java
@@ -0,0 +1,22 @@
+import java.io.*;
+import java.util.*;
+
+class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int N = Integer.parseInt(br.readLine());
+        int[] arr = new int[N];
+        int sum = 0;
+
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        for(int i = 0; i < N; i++) {
+            arr[i] = Integer.parseInt(st.nextToken());
+            sum += arr[i];
+        }
+
+        Arrays.sort(arr);
+
+        System.out.print(sum - arr[N - 1]);
+    }
+}
