2d1247478e4fcd836bf2aadf138d79ffb15e188f
sjhjack <102958758+sjhjack@users.noreply.github.com>
2025-02-05 17:02:31 +0900

    [Silver IV] Title: 인사성 밝은 곰곰이, Time: 276 ms, Memory: 28640 KB -BaekjoonHub

---
+++ 백준/Silver/25192. 인사성 밝은 곰곰이/인사성 밝은 곰곰이.java
@@ -0,0 +1,29 @@
+import java.io.*;
+import java.util.*;
+
+class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int N = Integer.parseInt(br.readLine());
+        Set<String> set = null;
+        int ans = 0;
+
+        for(int i = 0; i < N; i++) {
+            String s = br.readLine();
+
+            if(set == null && !s.equals("ENTER")) {
+                continue;
+            }
+
+            if(s.equals("ENTER")) {
+                set = new HashSet<>();
+            } else if(!set.contains(s)) {
+                ans++;
+                set.add(s);
+            }
+        }
+
+        System.out.print(ans);
+    }
+}
