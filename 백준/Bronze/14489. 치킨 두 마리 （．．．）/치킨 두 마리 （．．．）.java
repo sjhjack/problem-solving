d4dc0c4c5028981414c3a97587ab7449af43a3b9
sjhjack <102958758+sjhjack@users.noreply.github.com>
2025-03-06 22:08:13 +0900

    [Bronze IV] Title: 치킨 두 마리 (...), Time: 100 ms, Memory: 14112 KB -BaekjoonHub

---
+++ 백준/Bronze/14489. 치킨 두 마리 （．．．）/치킨 두 마리 （．．．）.java
@@ -0,0 +1,15 @@
+import java.io.*;
+import java.util.*;
+
+class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        int A = Integer.parseInt(st.nextToken());
+        int B = Integer.parseInt(st.nextToken());
+        int C = Integer.parseInt(br.readLine()) * 2;
+
+        System.out.print(A + B - C >= 0 ? A + B - C : A + B);
+    }
+}
