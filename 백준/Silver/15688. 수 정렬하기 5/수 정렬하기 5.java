ce9af50d43289cd86db389c57ea51b9cfe6db6be
sjhjack <102958758+sjhjack@users.noreply.github.com>
2025-06-12 20:25:50 +0900

    [Silver V] Title: 수 정렬하기 5, Time: 18008 ms, Memory: 110092 KB -BaekjoonHub

---
+++ 백준/Silver/15688. 수 정렬하기 5/수 정렬하기 5.java
@@ -0,0 +1,23 @@
+import java.io.*;
+import java.util.*;
+
+class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder ans = new StringBuilder();
+        int N = Integer.parseInt(br.readLine());
+        int[] arr = new int[N];
+
+        for(int i = 0; i < N; i++) {
+            arr[i] = Integer.parseInt(br.readLine());
+        }
+
+        Arrays.sort(arr);
+
+        for(int i = 0; i < N; i++) {
+            ans.append(arr[i]).append("\n");
+        }
+
+        System.out.print(ans);
+    }
+}
