eff891d1a5bca80ce14462ea030264d16014c9e3
sjhjack <102958758+sjhjack@users.noreply.github.com>
2025-06-24 19:38:32 +0900

    [Bronze II] Title: 래환이의 여자친구 사귀기 대작전, Time: 256 ms, Memory: 25008 KB -BaekjoonHub

---
+++ 백준/Bronze/32529. 래환이의 여자친구 사귀기 대작전/래환이의 여자친구 사귀기 대작전.java
@@ -0,0 +1,30 @@
+import java.io.*;
+import java.util.*;
+
+class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        int N = Integer.parseInt(st.nextToken());
+        int M = Integer.parseInt(st.nextToken());
+        int[] arr = new int[N];
+        int sum = 0;
+
+        st = new StringTokenizer(br.readLine());
+        for(int i = 0; i < N; i++) {
+            arr[i] = Integer.parseInt(st.nextToken());
+        }
+
+        for(int i = N-1; i >= 0; i--) {
+            sum += arr[i];
+
+            if(sum >= M) {
+                System.out.print(i + 1);
+                return;
+            }
+        }
+
+        System.out.print(-1);
+    }
+}
