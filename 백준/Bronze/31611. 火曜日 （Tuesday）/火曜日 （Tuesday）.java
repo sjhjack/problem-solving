800563aed1c030ce73c0f1129122f6a9df3ef32a
sjhjack <102958758+sjhjack@users.noreply.github.com>
2026-04-19 20:13:43 +0900

    [Bronze V] Title: 火曜日 (Tuesday), Time: 108 ms, Memory: 14280 KB -BaekjoonHub

---
+++ 백준/Bronze/31611. 火曜日 （Tuesday）/火曜日 （Tuesday）.java
@@ -0,0 +1,10 @@
+import java.io.*;
+
+class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int N = Integer.parseInt(br.readLine());
+
+        System.out.print(N % 7 == 2 ? "1" : "0");
+    }
+}
